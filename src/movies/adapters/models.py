c7db0afbf08a4e69d653e876346c0318583751cf
clarissavlsqz <clarissa.a.velasquez@gmail.com>
2022-05-29 20:41:38 -0500

    establish Repo Pattern

---
+++ src/movies/adapters/models.py
@@ -0,0 +1,58 @@
+import os
+from sqlalchemy import (
+    MetaData,
+    Column,
+    Integer,
+    String,
+    Float,
+    TIMESTAMP,
+    Text,
+    create_engine
+)
+from sqlalchemy.ext.declarative import declarative_base
+
+
+def get_postgres_uri():
+    host = os.environ.get("DB_HOST", "postgres")
+    port = 5432
+    password = os.environ.get("DB_PASS", "abc123")
+    user, db_name = "movies", "movies"
+    return f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
+
+
+Base = declarative_base(
+    metadata=MetaData(),
+)
+
+
+engine = create_engine(
+    get_postgres_uri(),
+    isolation_level="REPEATABLE READ",
+)
+
+
+class Movie(Base):
+    __tablename__ = "movies"
+
+    movie_id = Column(Integer, primary_key=True)
+    preference_key = Column(Integer)
+    movie_title = Column(String)
+    rating = Column(Float)
+    year = Column(Integer)
+    create_time = Column(TIMESTAMP(timezone=True), index=True)
+
+class User(Base):
+    __tablename__ = "users"
+
+    user_id = Column(Integer, primary_key=True)
+    preference_key = Column(Integer)
+    username = Column(String)
+    email = Column(String)
+
+    def __init__(self, preference_key, username, email) -> None:
+        self.preference_key = preference_key
+        self.username = username
+        self.email = email
+
+def start_mappers():
+    Base.metadata.create_all(engine)
